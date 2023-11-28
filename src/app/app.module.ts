f64d6fa48521c8a1da9f3872302c6551621494b4
Ted Hsu <ted.hsu@mitake.com.tw>
2023-11-28 18:49:18 +0800

    feat: Import core components into AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { SidenavComponent } from './core/components/sidenav/sidenav.component';
+import { FooterComponent } from './core/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderComponent,
+    SidenavComponent,
+    FooterComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
