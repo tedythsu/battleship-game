6d1825999b7cc754bc7cd1439447ce972104cc07
Ted Hsu <ted.hsu@mitake.com.tw>
2023-11-29 16:15:14 +0800

    feat: Add route for GamePageComponent in app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { GamePageComponent } from './pages/game-page/game-page.component';
 
 const routes: Routes = [
-  { path: '',   component: HomePageComponent, pathMatch: 'full' },
+  { path: '', component: HomePageComponent, pathMatch: 'full' },
+  { path: 'game', component: GamePageComponent },
   { path: '**', redirectTo: ''}
 ];
 
