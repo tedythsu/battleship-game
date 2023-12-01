f08f8e6b713eb727ad8cc8a9fe72ff298c3e1937
Ted Hsu <ted.hsu@mitake.com.tw>
2023-12-01 19:41:40 +0800

    feat: Update sidenav styles & router

--- src/app/core/components/sidenav/sidenav.component.ts
+++ src/app/core/components/sidenav/sidenav.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidenavService } from '../../services/sidenav.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
