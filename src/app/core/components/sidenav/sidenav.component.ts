f6af7b9f47c14d4593a9a278921e17c656d19402
Ted Hsu <ted.hsu@mitake.com.tw>
2024-01-09 18:46:09 +0800

    feat: Update sidenav Weather Information

--- src/app/core/components/sidenav/sidenav.component.ts
+++ src/app/core/components/sidenav/sidenav.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidenavService } from '../../services/sidenav.service';
 import { RouterModule } from '@angular/router';
+import { WeatherService, WeatherDataResponse } from '../../services/weather.service';
 
 @Component({
   selector: 'app-sidenav',
@@ -10,9 +11,46 @@ import { RouterModule } from '@angular/router';
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit {
 
-  constructor(private sidenav: SidenavService) { }
+  constructor(
+    private sidenav: SidenavService,
+    private weatherService: WeatherService
+  ) {}
+
+  weatherData: WeatherDataResponse = {
+    latitude: 0,
+    longitude: 0,
+    generationtime_ms: 0,
+    utc_offset_seconds: 0,
+    timezone: '',
+    timezone_abbreviation: '',
+    elevation: 0,
+    current_units: {
+      time: '',
+      interval: '',
+      temperature_2m: '',
+      precipitation: ''
+    },
+    current: {
+      time: '',
+      interval: 0,
+      temperature_2m: 0,
+      precipitation: 0
+    }
+  };
+
+  ngOnInit(): void {
+    this.getWeatherData();
+  }
+
+  private getWeatherData() {
+    this.weatherService.$getWeatherData().subscribe(
+      (data: WeatherDataResponse) => {
+        this.weatherData = data;
+      }
+    )
+  }
 
   get isSidenavOpen() {
     return this.sidenav.isSidenavOpen;
