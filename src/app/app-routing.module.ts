195fe9fd2c073137bcaa9ab7d60b942308a2beb9
Ted Hsu <ted.hsu@mitake.com.tw>
2024-01-19 17:32:02 +0800

    feat: update routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { GamePageComponent } from './pages/game-page/game-page.component';
+import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { AuthGuard } from './core/services/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent, pathMatch: 'full' },
-  { path: 'game', component: GamePageComponent },
+  { path: 'game', component: GamePageComponent, canActivate: [AuthGuard] },
+  { path: 'settings', component: SettingsPageComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'signup', component: SignupPageComponent },
   { path: '**', redirectTo: ''}
 ];
 
