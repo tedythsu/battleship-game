70c982a037b28a11bbfeb43f2606c3321f638580
Ted Hsu <ted.hsu@mitake.com.tw>
2024-06-04 19:24:44 +0800

    feat: disable 2 players game mode on mobile

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { AlertService } from 'src/app/core/services/alert.service';
 
 @Component({
   selector: 'app-home-page',
@@ -11,9 +12,17 @@ import { Router } from '@angular/router';
 })
 export class HomePageComponent {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private alertService: AlertService) {}
+
+  get isOnMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  }
 
   navigateToGame(gameMode: string) {
-    this.router.navigate(["game"], {state: {gameMode: gameMode}});
+    if (gameMode === 'Multi Player' && this.isOnMobileDevice) {
+      this.alertService.showModal(`2 players mode is not available on mobile device!`);
+    } else {
+      this.router.navigate(["game"], {state: {gameMode: gameMode}});
+    }
   }
 }
