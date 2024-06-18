8bc48e3d6d333330ae4507aa07e4a0242e6fa93c
Ted Hsu <ted.hsu@mitake.com.tw>
2024-06-18 16:41:53 +0800

    Add unit tests for initialization and game operations

--- src/app/pages/game-page/game-page.component.spec.ts
+++ src/app/pages/game-page/game-page.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GameMode, GamePageComponent } from './game-page.component';
+import { AlertService } from 'src/app/core/services/alert.service';
+import { signal } from '@angular/core';
 
 describe('GamePageComponent', () => {
   let component: GamePageComponent;
@@ -24,9 +26,7 @@ describe('GamePageComponent', () => {
   describe('Initialization', () => {
     describe('Single Player Mode', () => {
       beforeEach(() => {
-        // Arrange
         component.gameMode = GameMode.SINGLE_PLAYER;
-        // Act
         component.startGame(component.gameMode);
       })
 
@@ -64,16 +64,27 @@ describe('GamePageComponent', () => {
     });
 
     describe('Multiplayer Mode', () => {
+      beforeEach(() => {
+        component.gameMode = GameMode.MULTI_PLAYER;
+        component.startGame(component.gameMode);
+      })
+
       xit('should initialize game mode correctly for multiplayer mode', () => {
         // Test initialization of game mode, ensuring it defaults correctly for multiplayer mode
       });
 
-      xit('should generate players for multiplayer mode', () => {
-        // Test logic for generating players in multiplayer mode
+      it('should generate players correctly for multiplayer mode', () => {
+        const actualPlayersCount: number = component.players.length;
+        const expectedPlayersCount: number = 2;
+        // Assert
+        expect(actualPlayersCount).toBe(expectedPlayersCount);
       });
 
-      xit('should generate game boards for multiplayer mode', () => {
-        // Test logic for generating game boards in multiplayer mode
+      it('should generate game boards correctly for multiplayer mode', () => {
+        const actualBoardsCount: number = component.boards.length;
+        const expectedBoardsCount: number = 2;
+        // Assert
+        expect(actualBoardsCount).toBe(expectedBoardsCount);
       });
 
       xit('should initialize remaining ships for players in multiplayer mode', () => {
@@ -88,39 +99,118 @@ describe('GamePageComponent', () => {
 
   describe('Game Operations', () => {
     describe('Single Player Mode', () => {
-      xit('should execute shooting correctly in single player mode', () => {
-        // Test shooting functionality, including hit and miss scenarios in single player mode
+      beforeEach(() => {
+        component.gameMode = GameMode.SINGLE_PLAYER;
+        component.startGame(component.gameMode);
+      })
+
+      it('should mark the target cell as shot when shooting in single player mode', () => {
+        // Arrange
+        const boardIndex = 0;
+        const cellIndex = 0;
+        component.boards[boardIndex][cellIndex].hasBeenShot = false;
+        // Act
+        component.shoot(boardIndex, cellIndex);
+        // Assert
+        const cellHasBeenShot = component.boards[boardIndex][cellIndex].hasBeenShot;
+        expect(cellHasBeenShot).toBeTrue();
       });
 
       xit('should correctly check if a ship is sunk in single player mode', () => {
         // Test logic for checking if a ship is correctly sunk in single player mode
       });
 
-      xit('should handle game status correctly in single player mode', () => {
-        // Test handling of game status after the game ends in single player mode
+      it('should handle game status correctly when all enemy ships are sunk in single player mode', () => {
+        // Arrange
+        let alertService: AlertService;
+        alertService = TestBed.inject(AlertService);
+        spyOn(alertService, 'showModal');
+        component.isAnyPlayerAllShipsSunk = signal(true);
+        // Act
+        component.shoot(0, 0);
+        // Assert
+        const expectedMessage = 'VICTORY! ALL ENEMY SHIPS HAVE BEEN SUNK!';
+        expect(alertService.showModal).toHaveBeenCalledWith(expectedMessage);
       });
 
-      xit('should correctly decrease player missile count after shooting in single player mode', () => {
-        // Test whether player missile count is correctly decreased after shooting in single player mode
+      it('should handle game status correctly when player runs out of missiles in single player mode', () => {
+        // Arrange
+        let alertService: AlertService;
+        alertService = TestBed.inject(AlertService);
+        spyOn(alertService, 'showModal');
+        component.players[0].missileCount.set(1);
+        // Act
+        component.shoot(0, 0);
+        // Assert
+        const expectedMessage = 'GAME OVER: OUT OF MISSILE';
+        expect(alertService.showModal).toHaveBeenCalledWith(expectedMessage);
+      });
+
+      it('should correctly decrease player missile count after shooting in single player mode', () => {
+        // Arrange
+        const initialMissilesCount: number = component.players[0].missileCount();
+        // Act
+        component.shoot(0, 0);
+        // Assert
+        const updatedMissilesCount: number = component.players[0].missileCount();
+        expect(updatedMissilesCount).toBe(initialMissilesCount - 1);
       });
     });
 
     describe('Multiplayer Mode', () => {
-      xit('should execute shooting correctly in multiplayer mode', () => {
-        // Test shooting functionality, including hit and miss scenarios in multiplayer mode
+      beforeEach(() => {
+        component.gameMode = GameMode.MULTI_PLAYER;
+        component.startGame(component.gameMode);
+      })
+
+      it('should mark the target cell as shot when shooting in multiplayer mode', () => {
+        // Arrange
+        const boardIndex = 1
+        const cellIndex = 0;
+        component.boards[boardIndex][cellIndex].hasBeenShot = false;
+        // Act
+        component.shoot(boardIndex, cellIndex);
+        // Assert
+        const cellHasBeenShot = component.boards[boardIndex][cellIndex].hasBeenShot;
+        expect(cellHasBeenShot).toBeTrue();
       });
 
       xit('should correctly check if a ship is sunk in multiplayer mode', () => {
         // Test logic for checking if a ship is correctly sunk in multiplayer mode
       });
 
-      xit('should handle game status correctly in multiplayer mode', () => {
-        // Test handling of game status after the game ends in multiplayer mode
+      xit('should handle game status correctly when all ships of one player are sunk in multiplayer mode', () => {
+        // Test game status handling when all ships of one player are sunk
       });
 
-      xit('should correctly switch player turns after shooting in multiplayer mode', () => {
-        // Test whether player turns are correctly switched after shooting in multiplayer mode
+      it('should correctly switch player turns from player 1 to player 2 after shooting in multiplayer mode', () => {
+        // Arrange
+        const cellIndex = 0;
+        const playerOneIndex = 0;
+        const playerTwoIndex = 1;
+        component.players[playerOneIndex].isTurn = true;
+        component.players[playerTwoIndex].isTurn = false;
+        // Act
+        component.shoot(playerTwoIndex, cellIndex);
+        // Assert
+        expect(component.players[playerOneIndex].isTurn).toBeFalse();
+        expect(component.players[playerTwoIndex].isTurn).toBeTrue();
       });
+
+      it('should correctly switch player turns from player 2 to player 1 after shooting in multiplayer mode', () => {
+        // Arrange
+        const cellIndex = 0;
+        const playerOneIndex = 0;
+        const playerTwoIndex = 1;
+        component.players[playerOneIndex].isTurn = false;
+        component.players[playerTwoIndex].isTurn = true;
+        // Act
+        component.shoot(playerOneIndex, cellIndex);
+        // Assert
+        expect(component.players[playerOneIndex].isTurn).toBeTrue();
+        expect(component.players[playerTwoIndex].isTurn).toBeFalse();
+      });
+
     });
   });
 });
